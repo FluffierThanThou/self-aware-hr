7bd34c09e36aca588f752daa49406a627b7284db
FluffierThanThou <fluffy.l2032@gmail.com>
2021-05-13 00:17:04 +0200

    find optimal assignment and missing/extra specializations, implemented naive release & fire handlers

--- Extensions.cs
+++ Extensions.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright Karel Kroeze, 2021-2021.
-// SelfAwareHR/Extensions.cs
+// SelfAwareHR/SelfAwareHR/Extensions.cs
 
 using System;
 using System.Collections.Generic;
@@ -11,11 +11,6 @@ namespace SelfAwareHR
 {
     public static class Extensions
     {
-        public static SelfAwareHR SelfAwareHR(this Team team)
-        {
-            return global::SelfAwareHR.SelfAwareHR.For(team);
-        }
-
         public static void Destroy(this Object obj)
         {
             if (obj != null)
@@ -24,14 +19,19 @@ namespace SelfAwareHR
             }
         }
 
-        public static T Mode<T>(this IEnumerable<T> list, T defaultValue = default)
+        public static float DevTime(this SoftwareWorkItem.FeatureProgress feature, Employee.EmployeeRole role)
         {
-            if (list.IsNullOrEmpty())
+            switch (role)
             {
-                return defaultValue;
+                case Employee.EmployeeRole.Programmer:
+                    return feature.CDevTime;
+                case Employee.EmployeeRole.Designer:
+                    return feature.DevTime;
+                case Employee.EmployeeRole.Artist:
+                    return feature.ADevTime;
+                default:
+                    return 0;
             }
-
-            return list.Mode(item => item, defaultValue);
         }
 
         public static IEnumerable<T> FilterNull<T>(this IEnumerable<T> list)
@@ -47,6 +47,60 @@ namespace SelfAwareHR
         public static bool IsNullOrEmpty<T>(this IEnumerable<T> list)
         {
             return list == null || !list.Any();
+        }
+
+        public static string Join<T>(this IEnumerable<T> list, string sep = ", ", Func<T, string> stringifier = null)
+        {
+            return stringifier != null ? string.Join(sep, list.Select(stringifier)) : string.Join(sep, list);
+        }
+
+        public static string Join(this IEnumerable<string> list, string sep = ", ")
+        {
+            return string.Join(sep, list);
+        }
+
+        public static T Mode<T>(this IEnumerable<T> list, T defaultValue = default)
+        {
+            if (list.IsNullOrEmpty())
+            {
+                return defaultValue;
+            }
+
+            return list.Mode(item => item, defaultValue);
+        }
+
+        public static bool RelevantFor(this SoftwareWorkItem.FeatureProgress task,
+                                       Employee.EmployeeRole                 role,
+                                       bool                                  forceFull = false)
+        {
+            switch (role)
+            {
+                case Employee.EmployeeRole.Designer:
+                    return task.DevTime > 0 && (!task.CodeDone || forceFull);
+                case Employee.EmployeeRole.Programmer:
+                    return task.CDevTime > 0 && (!task.CodeDone || forceFull);
+                case Employee.EmployeeRole.Artist:
+                    return task.ADevTime > 0 && (!task.ArtDone || forceFull);
+                default:
+                    return false;
+            }
+        }
+
+        public static SelfAwareHR SelfAwareHR(this Team team)
+        {
+            return global::SelfAwareHR.SelfAwareHR.For(team);
+        }
+
+        public static List<T> Splice<T>(this List<T> list, int start, int count)
+        {
+            var sub = list.Skip(start).Take(count).ToList();
+            list.RemoveRange(start, count);
+            return sub;
+        }
+
+        public static List<T> Splice<T>(this List<T> list, int count)
+        {
+            return Splice(list, 0, count);
         }
 
         public static bool TryLoc(this string input, out string output)
@@ -73,48 +127,6 @@ namespace SelfAwareHR
 
             output = null;
             return false;
-        }
-
-        public static bool RelevantFor(this SoftwareWorkItem.FeatureProgress task,
-                                       Employee.EmployeeRole                 role,
-                                       bool                                  forceFull = false)
-        {
-            switch (role)
-            {
-                case Employee.EmployeeRole.Designer:
-                    return task.DevTime > 0 && (!task.CodeDone || forceFull);
-                case Employee.EmployeeRole.Programmer:
-                    return task.CDevTime > 0 && (!task.CodeDone || forceFull);
-                case Employee.EmployeeRole.Artist:
-                    return task.ADevTime > 0 && (!task.ArtDone || forceFull);
-                default:
-                    return false;
-            }
-        }
-
-        public static string Join<T>(this IEnumerable<T> list, string sep = ", ", Func<T, string> stringifier = null)
-        {
-            return stringifier != null ? string.Join(sep, list.Select(stringifier)) : string.Join(sep, list);
-        }
-
-        public static string Join(this IEnumerable<string> list, string sep = ", ")
-        {
-            return string.Join(sep, list);
-        }
-
-        public static float DevTime(this SoftwareWorkItem.FeatureProgress feature, Employee.EmployeeRole role)
-        {
-            switch (role)
-            {
-                case Employee.EmployeeRole.Programmer:
-                    return feature.CDevTime;
-                case Employee.EmployeeRole.Designer:
-                    return feature.DevTime;
-                case Employee.EmployeeRole.Artist:
-                    return feature.ADevTime;
-                default:
-                    return 0;
-            }
         }
     }
 }
